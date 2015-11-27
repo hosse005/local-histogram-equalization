0ddbec659db7a614b4ec71df1e14a6b2d2ffd0b1
Evan Hosseini <hosse005@umn.edu>
2015-11-27 16:53:33 -0600

    Local histogram equalization appears to be working

--- histogramEqualization.py
+++ histogramEqualization.py
@@ -18,7 +18,8 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 # Global parameters
-max_intensity = 255  # standard 8 bit grayscale max
+max_intensity = 255   # standard 8 bit grayscale max
+local_lower_limit = 3 # lower limit on local histogram
 
 # Entry point
 def main():
@@ -40,9 +41,19 @@ def main():
                         help='Output File Name', required=False,
                         default='contrastEnhancedImage.jpg')
 
+    # Local histogram neighborhood size, n
+    parser.add_argument('-n', dest='n', help='Size of local neighborhood (nxn)',
+                        type=int, required=False, default=0)
+
     args = parser.parse_args()
     inputFile = args.inputFile
     outputFile = args.outputFile
+    n = args.n
+
+    # Validate n w/ lower bound
+    if n is not 0 and n < local_lower_limit:
+        print('n parameter must be greater than %d!' % local_lower_limit - 1)
+        sys.exit()
 
     # Try to read the passed in image
     try:
@@ -51,48 +62,83 @@ def main():
         print('Unable to open input file %s!' % inputFile)
         sys.exit()
 
+    # Generate a time stamp for performance monitoring
+    t0 = time.time()
+
     # Load image data into an 8 bit numpy array
     data = np.uint8(np.array(img))
 
     # Equalize histogram
-    eqData = equalizeHistogram(data)
+
+    # If n is set, we perform local equalization, otherwise global
+    if n is not 0:
+        eqData = localEq(data, n)
+    else:
+        eqData = globalEq(data)
+
     hist, _ = np.histogram(eqData, bins=max_intensity+1,
                            range=(0,max_intensity))
 
+    # Generate end time stamp and report calculation time
+    t1 = time.time()
+    print('Processing complete in %3.2f seconds' % (t1 - t0))
+    
     plotHistogram(hist, 'Output Histogram')
 
     # Write histogram equalized output to file
     img = Image.fromarray(eqData)
     img.save(outputFile)        
 
-# Histogram equalization
-def equalizeHistogram(data):
+# Local Histogram equalization
+def localEq(data, n):
     '''
-    @param data : numpy 2d input array
+    @param data    : numpy 2d input array
+    @param n       : local histogram neighborhood size n in x and y
+    @return eqData : numpy 2d array - local histogram equalized output
+    '''
+    eqData = np.zeros(data.shape, np.uint8)
+    tmp = list()
+    
+    # Loop over each pixel in the image
+    for (x,y), value in np.ndenumerate(data):
+
+        # Collect the local neighborhood into tmp
+        for i in range(0,n):
+            s_x = x - int(n/2) + i
+            for j in range(0,n):
+                s_y = y - int(n/2) + j
+                if s_x >= 0 and s_x < data.shape[0] and \
+                   s_y >= 0 and s_y < data.shape[1]:
+                    tmp.append(data[s_x][s_y])
+
+        # Calculate the histogram transformation
+        s_k = calcTransform(np.asarray(tmp))
+
+        # Lookup the transformation for the given pixel
+        eqData[x,y] = s_k[value]
+
+        # Clear tmp for the next iteration
+        tmp = []
+
+    return eqData
+
+# Histogram equalization
+def globalEq(data):
+    '''
+    @param data    : numpy 2d input array
     @return eqData : numpy 2d array - histogram equalized output
     '''
     eqData = np.zeros(data.shape, np.uint8)
 
-    nPixels = data.shape[0] * data.shape[1]
-
-    # First calculate the histogram of the input
+    # Calculate the histogram of the input for plotting
     hist, _ = np.histogram(data, bins=max_intensity+1,
                            range=(0,max_intensity))
 
-    assert hist.sum() / nPixels == 1, 'Unexpected input data format!'
     plotHistogram(hist, 'Input Histogram')
     
     # Store the transformation in s_k
-    s_k = np.zeros(max_intensity + 1)
-    for idx, _ in enumerate(s_k):
-        s_k[idx] = max_intensity / nPixels * hist[:idx+1].sum()
-
-    # Round the transformation to nearest integer
-    s_k = np.rint(s_k)
-    for idx, value in np.ndenumerate(s_k):
-        if value > max_intensity:
-            s_k[idx] = max_intensity
-
+    s_k = calcTransform(data)
+    
     plotHistogram(s_k, 's_k')
 
     for (x,y), value in np.ndenumerate(data):
@@ -100,20 +146,30 @@ def equalizeHistogram(data):
 
     return eqData
 
-# Function for calcuating the histogram
-def calcHistogram(data):
+# Transformation helper function
+def calcTransform(data):
     '''
-    @param data : input array to calculate histogram
-    @return hist : histogram array
+    @param data : numpy array
+    @return s_k : numpy array of transformation coefficients
     '''
-    hist = np.zeros(max_intensity+1, dtype=np.uint8)
+    nPixels = data.size
 
-    for value in data:
-        idx = np.uint8(value)
-        hist[idx] = hist[idx] + 1
+    # First calculate the histogram of the input
+    hist, _ = np.histogram(data, bins=max_intensity+1,
+                           range=(0,max_intensity))
 
-    return hist
+    assert hist.sum() == nPixels, 'Unexpected input data format!'
     
+    # Store the transformation in s_k
+    s_k = np.zeros(max_intensity + 1)
+    for idx, _ in enumerate(s_k):
+        s_k[idx] = max_intensity / nPixels * hist[:idx+1].sum()
+
+    # Round the transformation down to nearest integer
+    s_k = np.floor(s_k)
+
+    return s_k
+
 # Histogram plotter
 def plotHistogram(data, title):
     '''
@@ -129,12 +185,8 @@ def plotHistogram(data, title):
     try:
         histFile = title + '.jpg'
         plt.savefig(histFile)
-        histFile = title + '.svg'
-        plt.savefig(histFile, format='svg', dpi=1200)
     except IOError:
         print('Unable to write histogram to file %s!' % histFile)
-              
-    #plt.show()    
-        
+                      
 if __name__ == '__main__':
     main()
